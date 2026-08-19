d3b8cce24c94e03dcce725e5928e22c0865097d6
ShantanuDas15 <shantanucool1361@gmail.com>
2026-08-19 22:15:16 +0530

    fix(frontend): configure Vite to load environment variables from root directory

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
+  envDir: '../',
   plugins: [react()],
   resolve: {
     alias: {
